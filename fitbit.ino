1e1ac3f9344e7a81b18a8fdc0b2c6957bc745cfd
Eric Born <eric.born85@gmail.com>
2022-02-18 14:41:57 -0600

    Update fitbit.ino

--- fitbit.ino
+++ fitbit.ino
@@ -13,7 +13,7 @@ const int stepsPerRevolution = 2800;
 
 // define start and end time
 const int startTime = 10;
-const int endTime = 13;
+const int endTime = 12;
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
@@ -23,7 +23,7 @@ Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
 void checkTime () {
   DateTime now = myRTC.now();
   //Serial.println(now.hour());
-  if (now.hour() >= startTime && now.hour() < endTime) {
+  if (now.hour() >= startTime && now.hour() <= endTime) {
     Serial.println("Between 11 and 13");
     // Step one revolution in one direction:
     //Serial.println("clockwise");
